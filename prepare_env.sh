32a00eac57bb97afdb75c8cf7eede333e723f8ab
kenji89 <mruczek89@gmail.com>
2015-06-05 19:04:52 +0200

    changed theme

--- prepare_env.sh
+++ prepare_env.sh
@@ -6,4 +6,5 @@ sudo npm install -g bower
 sudo npm install -g http-server
 bower install --allow-root
 (crontab -l ; echo "@reboot sh ~/websites/my_site/runServer.sh") | sort | uniq | crontab -
+(crontab -l ; echo "*/30 * * * * sh ~/websites/my_site/runServer.sh") | sort | uniq | crontab -
 echo "You should now reboot the system"
